db94c58f634336b635845fd37fa7974f080ff18f
Amy Booth <amybooth1221@gmail.com>
2021-12-02 18:47:14 -0500

    Update Autonomous_Test.java

--- Autonomous_Test.java
+++ Autonomous_Test.java
@@ -1,3 +1,8 @@
+/* 
+Hello!  I am going to comment directly both with additional comments from me as well as some comments I received from Coach Joe.  Please make changes
+as you see fit and send an email to let me know to look at changes.  Thank you!  Amy
+*/
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -6,6 +11,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "Placing a block")
 public class Autonomous_Test extends LinearOpMode {
+
+/* For the declarations below, think about what you actually need.  Because this is an autonomous program, will you need all of these?
+From Coach Joe:  *** He needs to rename the motors to more appropriate/descriptive names .  This is a MUST!
+*/
+
     DcMotor motor1;
     boolean a, b;
     double x;
@@ -23,6 +33,7 @@ public class Autonomous_Test extends LinearOpMode {
     DcMotor backLeft;
     DcMotor backRight;
     DcMotor wheel;
+    
     @Override
     public void runOpMode() throws InterruptedException {
         //Declaring motors through hardwareMap
@@ -33,6 +44,14 @@ public class Autonomous_Test extends LinearOpMode {
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
         wheel = hardwareMap.dcMotor.get("wheel");
+        
+/* 
+Why are we switching between RUN_USING_ENCODER and RUN_TO_POSITION?  What is the difference between the two?  When should you use one over the other?
+
+From Coach Joe:
+RUN_TO_POSITION = go to the exact position and stay there. (more accurate)
+RUN_USING_ENCODERS = get to the position as fast as possible and then stay if yoiu can (speed is more important than accuracy)
+*/       
         //Encoders reset and initialized
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -48,6 +67,14 @@ public class Autonomous_Test extends LinearOpMode {
         
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+/* 
+From previous email:  Before your waitForStart() statement, I would definitely recommend adding some telemetry statements to confirm that the encoders 
+for the motors that you want to reset are actually reset. 
+
+From Coach Joe:
+*** agreed, anytime he is using encoders (especially on things with hard stops like the lift) he should use telemetry atatements.
+*/
+        
         waitForStart();
         //Move lift up 1444 ticks, or to where the top level is
         movingvoid(1444, motor);
@@ -61,34 +88,45 @@ public class Autonomous_Test extends LinearOpMode {
         sleep(500);
         motor1.setPower(0);
     }
+    /* 
+    I moved your previous comments to where I think you mean for them to be in the code.
+    
+    From previous email:  we don't want the constraints for the chassis motors to be the same as the constraints for the 
+    cascading lift.  Think about how you might want to fix this :)
+    
+    Coach Joe sent me a comment about what he thinks you should do, but I want you to think it over first and let me know what
+    you are thinking.
+    
+    Other questions:  Why is ticks a double being passed in, but then is used as an int?  Why not just declare it an int?
+    
+    From Coach Joe: *** unless there is a specific reason to change between the modes, he should set the motor to one mode 
+    and leave it there instead of switching back and forth between RUN_TO_POSITION and RUN_USING_ENCODERS
+    
+    Also from Coach Joe:  *** he should not use abs.  he should make sure the value is positive.  Plus he should use the 
+    range/clip function to ensure it is within the correct range
+
+    */
+    
     public void movingvoid(double ticks, DcMotor motorbeingused){
         int newTarget;
         double speed2 = 1;
+        //make sure opmode is active
         if(opModeIsActive() && motorbeingused.getCurrentPosition() >= 0 && motorbeingused.getCurrentPosition() <= 3190){
+            //find current position and add distance to move
             newTarget = motorbeingused.getCurrentPosition() + (int)ticks;
+            // set target position of motor
             motorbeingused.setTargetPosition(newTarget);
+            //run to that position
             motorbeingused.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            // make sure that power is not negative (abs)
             motorbeingused.setPower(Math.abs(speed2));
 
-
+            // stop all motors
             motorbeingused.setPower(0);
+            // set to run using encoders
             motorbeingused.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             telemetry.addData("Height", motorbeingused.getCurrentPosition());
             telemetry.update();
         }
-        /*
-        make sure opmode is active
-        find current position and add distance to move
-        set target position of motor
-        run to that position
-
-        make sure that power is not negative (abs)
-
-        stop all motors
-
-        set to run using encoders
-
-        wait a quarter second (for accuracy, so that the program doesn't make the robot move in an unwanted direction)
-        */
     }
 }
